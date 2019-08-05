4223effb169409a7392daf84a184e96bce401dc1
Joseph <sungfuchiu@gmail.com>
2019-08-05 18:09:56 +0800

    Initialize a vue photo album

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,45 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Index from '@/components/Index'
+import Login from '@/components/Login'
+import SignUp from '@/components/SignUp'
+import Show from '@/components/Show'
+import New from '@/components/New'
+import Edit from '@/components/Edit'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/Index',
+      name: 'Index',
+      component: Index
+    },
+    {
+      path: '/Login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/SignUp',
+      name: 'SignUp',
+      component: SignUp
+    },
+    {
+      path: '/Show',
+      name: 'Show',
+      component: Show
+    },
+    {
+      path: '/Edit',
+      name: 'Edit',
+      component: Edit
+    },
+    {
+      path: '/New',
+      name: 'New',
+      component: New
     }
   ]
 })
